f0f15e04e578c8f61130ab7ab97965657f646373
notaurelle <aurelia.sibuea@gmail.com>
2024-07-28 20:14:32 +1200

    test commit

--- Assets/Scripts/PlayerDPS.cs
+++ Assets/Scripts/PlayerDPS.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class PlayerDPS : PlayerParent
 {
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("impact registered"); 
+    } 
     public override void PerformAbility()
     {
         base.PerformAbility();
 
         Debug.Log("DPS!");
     }
+
+
 }
