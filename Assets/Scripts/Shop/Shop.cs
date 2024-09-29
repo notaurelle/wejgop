cb00ff524d8de85e955fa51e70c1e01ba9e3b780
notaurelle <aurelia.sibuea@gmail.com>
2024-09-30 09:35:15 +1300

    playtest ver
    -locked out shop stuff

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Shop : MonoBehaviour
-{
-    public Image candyImage;
-    public Image itemImage;
-    public Text itemDescriptionText;
-    public Text itemCostText;
+//public class Shop : MonoBehaviour
+//{
+//    public Image candyImage;
+//    public Image itemImage;
+//    public Text itemDescriptionText;
+//    public Text itemCostText;
 
-    public ShopItem shopItem;
+//    public ShopItem shopItem;
 
-    public List<ShopItem> items = new List<ShopItem>();
-
-
-    //private void Update()
-    //{
-    //    if (isPlayerInRange && Input.GetKeyDown(KeyCode.F))
-    //    {
-    //        if (questWindow.activeSelf)
-    //        {
-    //            CloseQuestWindow();
-    //        }
-    //        else
-    //        {
-    //            OpenShopMenu();
-    //        }
-    //    }
-    //}
+//    public List<ShopItem> items = new List<ShopItem>();
 
 
+//    //private void Update()
+//    //{
+//    //    if (isPlayerInRange && Input.GetKeyDown(KeyCode.F))
+//    //    {
+//    //        if (questWindow.activeSelf)
+//    //        {
+//    //            CloseQuestWindow();
+//    //        }
+//    //        else
+//    //        {
+//    //            OpenShopMenu();
+//    //        }
+//    //    }
+//    //}
 
-    public void OpenShopMenu()
-        {
-         shopItem.item();
-           
-        }
 
 
-}
+//    public void OpenShopMenu()
+//    {
+//        shopItem.item();
+
+//    }
+
+
+//}
--- Assets/Scripts/Shop/ShopItem.cs
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopItem 
-{
-    public string itemDescription;
-    public string itemCost;
-    public Text itemDescriptionText;
-    public Text itemCostText;
+//public class ShopItem 
+//{
+//    public string itemDescription;
+//    public string itemCost;
+//    public Text itemDescriptionText;
+//    public Text itemCostText;
 
-    public void item()
-    {
-        itemDescriptionText.text = shopItem.itemDescription;
-        itemCostText.text = shopItem.itemCost;
-    }
+//    public void item()
+//    {
+//        itemDescriptionText.text = shopItem.itemDescription;
+//        itemCostText.text = shopItem.itemCost;
+//    }
 
-}
+//}
