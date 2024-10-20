4c2192b747aaf9210856d9c8087988889b7d4a11
notaurelle <aurelia.sibuea@gmail.com>
2024-10-21 12:04:08 +1300

    Trigger for in level dialogue cutscenes
    - adjustments to cathys script

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -7,22 +7,42 @@ public class DialogueTrigger : MonoBehaviour
     public DialogueManager dialogueManager;
     public Dialogue[] dialogue;
 
+    public BoxCollider2D boxCollider;
+    //public GameObject pause;
+    public GameObject next;
+
     public bool startOnTriggerEnter;
 
     public void TriggerDialogue()
     {
         dialogueManager.StartDialogue(dialogue);
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(startOnTriggerEnter && !dialogueManager.isRunning)
+        if (startOnTriggerEnter && !dialogueManager.isRunning)
         {
             if (other.CompareTag("Wylla") || other.CompareTag("Ally"))
             {
                 dialogueManager.StartDialogue(dialogue);
+
+                boxCollider = other.GetComponent<BoxCollider2D>();
+                if (boxCollider != null)
+                {
+                    boxCollider.enabled = false;
+
+                    next.SetActive(true);
+
+                    //if (!dialogueManager.isRunning)
+                    //{
+                    //    next.SetActive(false);
+                    //}
+                    ////pause.SetActive(false);
+                }
             }
         }
-       
     }
+
 }
